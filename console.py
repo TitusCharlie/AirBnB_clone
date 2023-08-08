b4f331b11ede85a6c75ec9e8356054c07e5cd70b
tituscharlie <ituslifecharlie>
2023-08-08 13:38:37 +0100

    console set up comleted

---
+++ console.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+==========================
+    Base module for AirBnB
+==========================
+"""
+from cmd import Cmd as command;
+import sys
+
+
+"""
+=====================================
+    create a class command line input
+===================================== 
+"""
+
+
+class tlcAirbnb(command):
+    prompt = '(hbnb) '
+
+    # def __init__(self, args):
+        # super.__init__():
+
+    # def do_create(self, args):
+    #     """Creates a new AirBnB object based on the argument"""
+
+    # def do_update(self, args):
+    #     """Update an existing object"""
+
+    # def do_delete(self, args):
+    #     """delete an existing object"""
+
+    # def do_show(self, args):
+    #     """show the details of an object"""
+
+    def do_EOF(self, args):
+        """End of files"""
+        return True
+
+    def do_help(self, args):
+        """Help documentation"""
+        
+        command.do_help(self, args)
+        
+    def do_quit(self, args):
+        """quit the interpreter"""
+        return True
+
+    def emptyline(self) -> bool:
+        """returns the empty line"""
+        # return super().emptyline()
+        pass
+
+if __name__ == "__main__":
+
+    interpreter = tlcAirbnb()
+    interpreter.cmdloop()
