decb4da00aef8135794334f62416ec187fcca386
tituscharlie <ituslifecharlie>
2023-08-12 23:47:35 +0100

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,7 @@ import modules
 
 import json
 from models.base_model import BaseModel
+from models.user import User
 
 
 """
---
+++ models/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""import modules"""
+
+from base_model import BaseModel
+
+
+"""
+=====================================================
+    Represent a User.
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+=====================================================
+"""
+
+
+class User(BaseModel):
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
